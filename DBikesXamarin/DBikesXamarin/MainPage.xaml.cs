46c8099283a28e686e2e42a08633d5ecd200f74e
kevinwalsh <kevface91@gmail.com>
2020-05-30 21:10:27 +0100

    Adding Xamarin Mobile project, skeleton files. Omitting iOS as no available device for testing. (Note: project wouldn't build with dot separator "DBikes.Xamarin"

---
+++ DBikesXamarin/DBikesXamarin/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DBikesXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void HelloButtonClicked(object sender, EventArgs e)
+        {
+            string inputName = NameEntry.Text;
+             HelloLabel.Text = "hello " + inputName;
+            await DisplayAlert("Welcome!", "Nice to meet you, "+inputName, "Continue");
+        }
+
+    }
+}
