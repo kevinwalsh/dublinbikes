fde5a136a5c70532f45ec50952466475e2c3c220
kevinwalsh <kevface91@gmail.com>
2020-06-03 11:18:38 +0100

    Xamarin: Added conditional formatting & Valueconverters for low station availability

---
+++ DBikesXamarin/DBikesXamarin/Helpers/DataConverters/IsLessThanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DBikesXamarin
+{
+    public class IsLessThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Messy casts required, "value" needs casting from obj to int, "parameter" is in quotes so is trickier
+           if((int)value <= int.Parse(parameter.ToString())) { 
+                return true; 
+            }
+            else return false; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
